2ca9c4eb552a92634daf4e2e91c85ab83ad900e5
Glener Martins <glener.tech@gmail.com>
2026-07-17 19:19:37 -0300

    chore: initial commit

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+// src/assets/index.ts
+export { default as animacao } from '@/assets/animacao.svg';
+export { default as background } from '@/assets/background.svg';
+export { default as icon } from '@/assets/icon.svg';
+export { default as iconeBalanca } from '@/assets/icone-balanca.svg';
+export { default as iconeCheck } from '@/assets/icone-check.svg';
+export { default as iconeCoracao } from '@/assets/icone-coracao.svg';
+export { default as iconeRaio } from '@/assets/icone-raio.svg';
